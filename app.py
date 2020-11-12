c7086851153eed741867b1603937c72d516bf182
Nicholas Burns <nmburns@triad.rr.com>
2020-11-11 22:48:46 -0500

    edit

--- app.py
+++ app.py
@@ -86,6 +86,38 @@ def stations():
         return jsonify(all_stats)
 
 # Tobs route
+@app.route("/api/v1.0/tobs")
+def tobs():
+    """Return a list of TObs for most active station."""
+    
+    # Find the last data point and the year before it
+    last = sess.query(meas.date).order_by(meas.date.desc()).first()
+    year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    
+    # Find most active station
+    active = sess.query(meas.station, stat.name, func.count(meas.station))\
+        .group_by(meas.station)\
+        .order_by(func.count(meas.station).desc()).first()
+    
+    # Find the tobs most active station
+    most = sess.query(meas.date, meas.tobs)\
+        .filter(meas.date > year)\
+        .filter(meas.date < last)\
+        .filter(meas.station == active[0]).all()
+
+    # Create Dictionary
+    most_act = []
+    for t, da in most:
+        act_row = {}
+        act_row['Date'] = da
+        act_row['TObs'] = t
+        
+        # Return JSON representation
+        return jsonify(most_act)
+
+
+
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
